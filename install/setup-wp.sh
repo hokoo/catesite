117dc12e0fd0a40b7104eb7f0df0d9fd349740b8
Igor Tron <root@itron.pro>
2022-05-25 21:12:14 +0200

    setup script improved

--- install/setup-wp.sh
+++ install/setup-wp.sh
@@ -21,7 +21,26 @@ if [ ! -f wp-config.php ]; then
 fi
 
 # install WP
-echo "WP database init"
-wp core install --url=$PROJECT_BASE_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
+echo -n "Would you import database from db.sql (y), init new instance (i), or do nothing (n)? (y/n/i)"
+
+read item
+case "$item" in
+    y|Y)
+    echo "WP database import..."
+    wp db import db.sql
+      ;;
+
+    i|I)
+    echo "WP database init new instance..."
+    wp core install --url=$PROJECT_BASE_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
+    printf "WP User Admin: %s \nWP User Pass: %s\n" $WP_ADMIN $WP_ADMIN_PASS
+      ;;
+
+    *)
+      echo "WP database has not been touched."
+      ;;
+esac
+
+echo "Do not forget renew your hosts file 127.0.0.1 domain.loc"
 echo "Done."
-printf "WP User Admin: %s \nWP User Pass: %s\n" $WP_ADMIN $WP_ADMIN_PASS
+
