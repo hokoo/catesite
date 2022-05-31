bde898677f654de687c6e669431d98edd5ed825b
Igor Tron <root@itron.pro>
2022-05-31 19:52:02 +0200

    wp has been updated to 5.9

--- install/setup-wp.sh
+++ install/setup-wp.sh
@@ -6,14 +6,13 @@ echo "WP setup preparing..."
 . ./.env
 
 # prepare file structure
-[ ! -d ./wp/ ] && mkdir -p ./wp/ && cp -r ./vendor/wordpress/wordpress/* ./wp
 [ ! -d ./wp-content/ ] && mkdir -p ./wp-content/
-cp -r ./vendor/wordpress/wordpress/wp-content/* ./wp-content
+cp -r ./wordpress/wp-content/* ./wp-content
 
 [ ! -f ./index.php ] && echo "<?php
 // WordPress view bootstrapper
 define( 'WP_USE_THEMES', true );
-require( './wp/wp-blog-header.php' );" > index.php
+require( './wordpress/wp-blog-header.php' );" > index.php
 
 if [ ! -f wp-config.php ]; then
   WPCONFIG=$(< ./install/.example/wp-config.php.template)
